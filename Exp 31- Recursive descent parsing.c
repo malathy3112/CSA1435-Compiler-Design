ca16a90e4b4d746cee8a5e191f7099082aafe017
malathy3112 <123725925+malathy3112@users.noreply.github.com>
2023-10-19 13:24:49 +0530

    Create Exp 31- Recursive descent parsing.c

---
+++ Exp 31- Recursive descent parsing.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int c=0;
+char p[20];
+void s();
+void l();
+void lprime();
+void l()
+{
+	s();
+	lprime();
+	
+}
+void lprime()
+{
+	if(p[c]==',')
+	{
+		c++;
+		s();
+		lprime();
+	}
+}
+void s()
+{
+	if(p[c]=='a')
+	c++;
+	else if(p[c]=='(')
+	{
+		c++;
+		l();
+		if(p[c]==')')
+		c++;
+		else
+		c--;
+	}
+	else
+	printf("\ninvalid expression");
+}
+int main()
+{
+	printf("\n implementation of recursive descent parser\n");
+	printf("\nenter the expression:\n");
+	scanf("%s",p);
+	s();
+	if(p[c]=='$')
+	printf("\nthe string is accepted");
+	else
+	printf("\n the string is rejected");
+}
+OUTPUT:
+Implement of recursive descent parsing
+Enter the expression:
+E -> TE'
+E' -> TE'/ε
+T -> FT'
+T' -> *FT'/ε
+F -> (E)/Id
+
+The String is Accepted.
